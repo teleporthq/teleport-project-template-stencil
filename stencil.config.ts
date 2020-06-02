00cb5cefaecb088f10fbea1c99889f97ec94d6c8
JayaKrishnaNamburu <namburu1995@gmail.com>
2020-06-02 09:52:04 +0530

    Stencil config change to support global styles

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,16 +1,16 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
+  namespace: "app",
+  globalStyle: "src/style.css",
   outputTargets: [
     {
-      type: 'www',
+      type: "www",
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
-    }
-  ]
+      baseUrl: "https://myapp.local/",
+    },
+  ],
 };
